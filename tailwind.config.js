a2cbee8c88da4917f9b18a94506877421b534c07
AH-sunny <sunnycse03@gmail.com>
2024-05-21 11:36:35 +0600

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'banner-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('./image/attachment_1.jpeg')",
+       },
+      },
     },
     plugins: [],
   }
